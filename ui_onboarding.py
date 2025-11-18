7073843dc2b2bb62d8d4e364fbea50cbc5b279bf
Prad0-2 <pradymanickam@gmail.com>
2025-11-18 12:30:41 +0530

    Debug: Hardcode Firebase config to isolate API error

--- ui_onboarding.py
+++ ui_onboarding.py
@@ -9,37 +9,25 @@ def show():
     """
     st.title("Welcome to The Dorian Engine")
 
-    required_env_vars = [
-        "FIREBASE_API_KEY",
-        "FIREBASE_AUTH_DOMAIN",
-        "FIREBASE_PROJECT_ID",
-        "FIREBASE_STORAGE_BUCKET",
-        "FIREBASE_MESSAGING_SENDER_ID",
-        "FIREBASE_APP_ID",
-        "FIREBASE_MEASUREMENT_ID",
-    ]
+    # --- TEMPORARY DEBUGGING STEP ---
+    # Hardcoding Firebase config to isolate the problem.
+    # If this works, the issue is with how secrets are being passed as environment variables.
+    firebase_config = {
+        "apiKey": "AIzaSyAaG7ynv1k3PgVvjc8SB2ieWwvcrZ6CwWI",
+        "authDomain": "dorian-engine-478520.firebaseapp.com",
+        "projectId": "dorian-engine-478520",
+        "storageBucket": "dorian-engine-478520.firebasestorage.app",
+        "messagingSenderId": "483770294230",
+        "appId": "1:483770294230:web:54a81c557606a0ede38e77",
+        "measurementId": "G-9046TJQ5WQ",
+        "databaseURL": "",
+    }
 
-    missing_vars = [var for var in required_env_vars if not os.environ.get(var)]
+    st.write("Please log in or sign up to continue.")
+    
+    user = fb_streamlit_auth(firebase_config)
 
-    if missing_vars:
-        st.error(f"The following required environment variables are missing: {', '.join(missing_vars)}")
-        st.error("Please ensure that the secrets are correctly configured and mounted in the Cloud Run service.")
-    else:
-        st.write("Please log in or sign up to continue.")
-        firebase_config = {
-            "apiKey": os.environ.get("FIREBASE_API_KEY"),
-            "authDomain": os.environ.get("FIREBASE_AUTH_DOMAIN"),
-            "projectId": os.environ.get("FIREBASE_PROJECT_ID"),
-            "storageBucket": os.environ.get("FIREBASE_STORAGE_BUCKET"),
-            "messagingSenderId": os.environ.get("FIREBASE_MESSAGING_SENDER_ID"),
-            "appId": os.environ.get("FIREBASE_APP_ID"),
-            "measurementId": os.environ.get("FIREBASE_MEASUREMENT_ID"),
-            "databaseURL": "",
-        }
-
-        user = fb_streamlit_auth(firebase_config)
-
-        if user:
-            st.session_state['authenticated'] = True
-            st.session_state['user_info'] = user
-            st.experimental_rerun()
+    if user:
+        st.session_state['authenticated'] = True
+        st.session_state['user_info'] = user
+        st.experimental_rerun()
